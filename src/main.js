456e26a402f3f4658b9c9f9c24f87e478d97076a
xmliszt <xmliszt@gmail.com>
2021-07-12 04:12:06 +0800

    chore: remove router, add deployment config

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import locale from "element-ui/lib/locale/lang/en";
@@ -10,6 +9,5 @@ Vue.use(ElementUI, { locale });
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   render: (h) => h(App),
 }).$mount("#app");
